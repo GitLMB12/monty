c7cc69bc4562f237e44e2a138d64bc297fce8736
GitLMB12 <mhamedbenzaquiii@gmail.com>
2024-01-21 05:45:22 -0800

    e

--- main.c
+++ main.c
@@ -1,12 +1,55 @@
 #include "monty.h"
-#include <stdio.h>
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
+ * custom_getline - Custom implementation of getline
+ * @lineptr: Pointer to the buffer
+ * @n: Pointer to the size of the buffer
+ * @stream: File stream to read from
+ * Return: Number of characters read, or -1 if there are no more lines
+ */
+ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
+{
+	size_t i;
+	int c;
+
+	if (*lineptr == NULL)
+	{
+		*n = 128;
+		*lineptr = (char *)malloc(*n);
+		if (*lineptr == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+	}
+	i = 0;
+	while ((c = fgetc(stream)) != EOF && c != '\n')
+	{
+		if (i >= *n - 1)
+		{
+			*n *= 2;
+			*lineptr = (char *)realloc(*lineptr, *n);
+			if (*lineptr == NULL)
+			{
+				perror("realloc");
+				exit(EXIT_FAILURE);
+			}
+		}
+		(*lineptr)[i++] = (char)c;
+	}
+	if (i == 0 && c == EOF)
+	{
+		return (-1);
+	}
+	(*lineptr)[i] = '\0';
+	return (i);
+}
+/**
+ * main - Monty code interpreter
+ * @argc: Number of arguments
+ * @argv: Monty file location
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
 	char *content;
@@ -31,7 +74,7 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
+		read_line = custom_getline(&content, &size, file);
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
@@ -42,5 +85,5 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-return (0);
+	return (0);
 }
